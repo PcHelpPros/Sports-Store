cb8bdb8a1d2b49dd7b1dfad059df56ee9f5662af
Justin Morris <PcHelpPros@Gmail.Com>
2018-05-29 10:30:03 -0400

    Added Abstract Folder Added Interface

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SportsStore.Domain.Abstract
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; set; }
+    }
+}
