09ceb00e22d65329a7dd9fcce1c9ca025bc61fe9
Justin Morris <PcHelpPros@Gmail.Com>
2018-05-30 11:28:57 -0400

    Created a new product repository

---
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SportsStore.Domain.Entities;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFDbContext : System.Data.Entity.DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
